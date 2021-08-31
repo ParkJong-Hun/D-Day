de54ef03d425972f4af344c81f1395125335c9ee
ParkJong-Hun <thesn515@gmail.com>
2021-08-31 19:14:18 +0900

    2021/08/31 19:14 Generate Proejct

---
+++ DDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DDay
+//
+//  Created by 박종훈 on 2021/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toolbar.item
+    }
+
+    @IBOutlet weak var toolbar: UIToolbar!
+    @IBOutlet weak var addBtn: UIBarButtonItem!
+}
+
