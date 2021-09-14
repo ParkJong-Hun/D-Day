668154121845fcc6d8bad70fd3f73c720a1d33ae
ParkJong-Hun <thesn515@gmail.com>
2021-09-14 21:58:02 +0900

    2021/09/14 21:57 Information View

--- DDay/Controller/InformationController.swift
+++ DDay/Controller/InformationController.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import UIKit
+
+class InformationController:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBOutlet weak var EventNameLabel: UILabel!
+    @IBOutlet weak var DDayLabel: UILabel!
+    @IBOutlet weak var DateLabel: UILabel!
+    @IBOutlet weak var CategoryImage: UIImageView!
+    @IBAction func clicked_edit_button(_ sender: Any) {
+        
+    }
+}
